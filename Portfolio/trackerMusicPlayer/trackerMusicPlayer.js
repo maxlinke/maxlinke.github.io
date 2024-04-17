39f8db111615c9bb696bba726d43e085c0ab0610
Max Linke <maxlinke@users.noreply.github.com>
2024-04-17 16:50:07 +0200

    added a new page for tracker music, hopefully a shorter one

---
+++ Portfolio/trackerMusicPlayer/trackerMusicPlayer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+registerPage("trackerMusicPlayer", {
+    title: "Tracker Music Player",
+    miniDescription: "A plugin that properly plays .mod and .xm music files in Unity",
+    year: 2023,
+    numberInYear: 1,
+    subfolderName: "trackerMusicPlayer",
+    backgroundImageName: "tempBackground.png",
+    createElements: () => {
+        // TODO
+    }
+});
