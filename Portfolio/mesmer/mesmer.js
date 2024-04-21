4fa1dc91b0353bfdcf5b8967060cc728eb6431cf
Max Linke <maxlinke@users.noreply.github.com>
2024-04-21 23:49:18 +0200

    forgot one important thing

---
+++ Portfolio/mesmer/mesmer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+registerPage("mesmer", {
+    title: "Mesmer",
+    miniDescription: "The first commercial game I worked on by interning at a game studio",
+    year: 2019,
+    numberInYear: 1,
+    subfolderName: "mesmer",
+    backgroundImageName: "../underConstruction.png",
+    createElements: () => {
+        addFormattedParagraph("TODO");
+    }
+});
