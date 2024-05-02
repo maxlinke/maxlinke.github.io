ac4b7512b09066f8352ce572f5ecd1d96f007bfb
Max Linke <maxlinke@users.noreply.github.com>
2024-05-02 14:13:38 +0200

    moved the matrix demo build

---
+++ Portfolio/incrementalComputer/incrementalComputer.js
@@ -0,0 +1,13 @@
+'use strict';
+
+registerPage ("incrementalComputer", {
+    title: "Incremental Computer",
+    miniDescription: "A small incremental game made for a course at uni",
+    year: 2022,
+    numberInYear: 20,
+    subfolderName: "incrementalComputer",
+    backgroundImageName: "../underConstruction.png",
+    createElements: function () {
+        
+    }
+});
